1c6cacb0e3bb714de93b7651db1f67f63a5b67ff
pqminh27 <minh8a1hb@gmail.com>
2023-03-15 15:57:03 +0300

    Minor fix for returning type while catch error

--- bot.py
+++ bot.py
@@ -27,6 +27,9 @@ def write_log_to_file_txt(file_name: str, msg: str): # Function to open error_lo
 def reset_data(arr: list):
     del arr[:-2]
 
+def clear_data(arr: list):
+    arr.clear()
+
 CONVERSATIONS = [] # History conversation
 def get_response_from_openai(user_input: str): # Define the function to get the response from the chat bot
     def get_response():
@@ -37,23 +40,23 @@ def get_response_from_openai(user_input: str): # Define the function to get the 
             reset_data(CONVERSATIONS) # The chatbot remembers its last 5 questions and 5 answers and then just the last question and answer 
             # raise openai.error.APIConnectionError("Connect to openai failed!")
         except Exception as error:
-            CONVERSATIONS.clear() # Clear cache data
+            clear_data(CONVERSATIONS) # Clear cache data
             current_time = show_time_now()
             error_msg = f"An error occurred while generating a response from OpenAI - at {current_time}: {error}\n"
             write_log_to_file_txt("error_log.txt", error_msg)
-            return "system", "I'm sorry, I was unable to generate a response. Please try again later!"
+            return "I'm sorry, I was unable to generate a response from OpenAI API. Please try again later!"
         
         try:
             choices_from_response_openai = response["choices"]
+            role_reply = choices_from_response_openai[0].message.role.strip()
+            content_reply = choices_from_response_openai[0].message.content.strip()
+            CONVERSATIONS.append({'role':role_reply, 'content':content_reply})
+            return content_reply
         except KeyError: # The KeyError occurs when a key specified in a dictionary is not found in the dictionary.
             current_time = show_time_now()
             write_log_to_file_txt("error_log.txt", f"An error occurred while 'extracting' the response from OpenAI, not found key 'choices' - at {current_time}: {response}\n")
-            return "system", "I'm sorry, I was unable to extract a response from the OpenAI API. Please try again later!"
-            
-        role_reply = choices_from_response_openai[0].message.role.strip()
-        content_reply = choices_from_response_openai[0].message.content.strip()
-        CONVERSATIONS.append({'role':role_reply, 'content':content_reply})
-        return content_reply
+            clear_data(CONVERSATIONS) # Clear cache data
+            return "I'm sorry, I was unable to extract a response from OpenAI API. Please try again later!"
     
     return get_response
  
@@ -67,12 +70,14 @@ def chat_handler(update: Update, context: CallbackContext): # Define the chat me
 
 def start_command(update: Update, context: CallbackContext): # Define the start command
     update.message.reply_text("Hi, I am a simple A.I chat bot! How can I help you today? '/help' for more info!")
+    clear_data(CONVERSATIONS) # Clear cache data for a new conversation
     
 def help_command(update: Update, context: CallbackContext): # Define the help command
     update.message.reply_text("I am a bot that can help you with the following commands: \n /start - Start the bot \n /help - Get this message :))) \n /end - End talking with the bot \nYou just need to write your message and I'll give you a response!")
     
 def end_command(update: Update, context: CallbackContext): # Define the end command to end the conversation
     update.message.reply_text("Bye! I hope we can talk again soon!")
+    clear_data(CONVERSATIONS) # Clear cache data for ending conversation
     return ConversationHandler.END
     
 def error_handler(update: Update, context: CallbackContext): # Define the error handler
